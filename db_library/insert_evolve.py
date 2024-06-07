e55b7a6addb3302a963945b4f862867e58f4532b
IvanChang0290 <ivanty2chang@gmail.com>
2024-06-07 23:04:16 +0800

    Add insert

--- db_library/insert_evolve.py
+++ db_library/insert_evolve.py
@@ -15,39 +15,38 @@ debug = 0
 
 with open('pokemons_evo.csv', newline='',encoding="utf-8") as csvfile:
     csv_data = csv.reader(csvfile)
-    next(csv_data) 
+    next(csv_data) # skip first row
     name_to_id = {row[1]: row[0] for row in csv_data if int(row[0]) <= 721}
 
 
 with open('pokemons_evo.csv', newline='',encoding="utf-8") as csvfile:
     csv_data = csv.reader(csvfile)
-    next(csv_data) 
+    next(csv_data) # skip first row
 
     for row in csv_data:
 
-        if int(row[0])>721:
+        if int(row[0])>721: # match limit
             break
 
 
-        if row[4]!="nothing":
+        if row[4]!="nothing": # if have evolve
             evolved_from = row[4]
 
             if evolved_from in name_to_id:
                 original_id = name_to_id[evolved_from]
 
-                if debug ==1:
-                    print(row[0],row[1], evolved_from, original_id)
+                if debug:
+                    print(row[0],row[1],original_id ,evolved_from ) #(evolve_id , evolve name , original_id , original name)
 
                 query='INSERT INTO Evolve(Evo_P_ID,Ori_P_ID) '+'VALUES('
                 new_data='"'+str(row[0])+'"'+','+'"'+str(original_id)+'"'
                 end=');'
                 new_query=query+new_data+end
 
-                if debug ==1:
+                if debug:
                     print(new_query)
                 # cursor.execute(new_query)
 
 
-#close the connection to the database.
 mydb.commit()
 
---
+++ db_library/insert_type.py
@@ -0,0 +1,54 @@
+import csv
+import numpy as np
+import mysql.connector
+
+#print(dir(mysql))
+
+
+mydb = mysql.connector.connect(user='root', password='police26',
+                              host='127.0.0.1',
+                              database='db_pokemon')
+cursor = mydb.cursor()
+
+debug = 0
+
+
+with open('pokemon_alopez247.csv', newline='',encoding="utf-8") as csvfile:
+    csv_data = csv.reader(csvfile)
+    next(csv_data)
+    
+    for row in csv_data:
+
+        query='INSERT INTO Pokemon_type(P_ID,type) '+'VALUES('
+        new_data='"'+str(row[0])+'"'+','+'"'+str(row[2])+'"'
+        end=');'
+
+        new_query=query+new_data+end
+
+        if debug:
+            print(row[0],row[2],row[3])
+
+        if debug:
+            print(new_query)
+
+        # cursor.execute(new_query)
+
+        if row[3] != '':
+            query='INSERT INTO Pokemon_type(P_ID,type) '+'VALUES('
+            new_data='"'+str(row[0])+'"'+','+'"'+str(row[3])+'"'
+            end=');'
+
+            new_query=query+new_data+end
+
+            if debug:
+                print(new_query)
+
+            # cursor.execute(new_query)
+
+
+
+
+          
+#close the connection to the database.
+mydb.commit()
+
