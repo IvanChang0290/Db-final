811f3d381a20a3fd834bcc057089fa1379c9993b
Chianghaoyu <a0981610711@gmail.com>
2024-06-08 19:48:35 +0800

    Update login.php

--- db_poke/login.php
+++ db_poke/login.php
@@ -20,14 +20,19 @@ if (isset($_POST['visitor'])) {
 // Retrieve form data
 $user = $_POST['username'];
 $pass = $_POST['password'];
-
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT, ['cost' => 12]);
+//echo "$user";
+//echo "$pass";
+echo $password;
 // Query to check user credentials
-$sql = "SELECT password FROM users WHERE username='$user'";
+$sql = "SELECT password FROM user WHERE name='$user'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    if (password_verify($pass, $row['password'])) {
+    $passHash = $row["Password"];
+    echo $passHash;
+    if (password_verify($pass, $row["Password"])) {
         echo "Login successful";
     } else {
         echo "Invalid password";
