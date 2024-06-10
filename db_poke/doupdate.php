ec21140530530066543cd377437a56715dd85a1d
DavidWuCode <131688347+DavidWuCode@users.noreply.github.com>
2024-06-10 19:51:11 +0800

    Update doupdate.php
    更改doupdate.php

--- db_poke/doupdate.php
+++ db_poke/doupdate.php
@@ -1,14 +1,15 @@
 <?php
+// doupdate.php
 
-// ******** update your personal settings ******** 
 $servername = "localhost";
 $username = "root";
 $password = "123456789";
 $dbname = "db_poke";
 
-// Connecting to and selecting a MySQL database
+// Connect MySQL server
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// set up char set
 if (!$conn->set_charset("utf8")) {
     printf("Error loading character set utf8: %s\n", $conn->error);
     exit();
@@ -18,25 +19,25 @@ if (!$conn->set_charset("utf8")) {
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-$B_ID = $_POST['B_ID'];
-	$P_ID = $_POST['P_ID'];
-	$S_ID = $_POST['S_ID'];
-if (isset($_POST['B_ID']) && isset($_POST['P_ID']) && isset($_POST['S_ID'])) {
 
-	session_start();
-	$update_sql = "UPDATE have SET S_ID='$S_ID' where B_ID='$B_ID' and P_ID='$P_ID'";	// TODO 
-	echo $update_sql;
-	if ($conn->query($update_sql) === TRUE) {
-		// 重定向用戶到下一頁
-		header('Location: display.php');
-		exit;
+session_start();
+$B_ID = $_SESSION["B_ID"];
+$P_ID = $_SESSION["P_ID"];
+$skill1 = $_POST['skill1'];
+$skill2 = $_POST['skill2'];
+$skill3 = $_POST['skill3'];
 
-	} else {
-		echo "<h2 align='center'><font color='antiquewith'>修改失敗!!</font></h2>";
-	}
+// Update skills
+$update_sql = "UPDATE have SET S_ID='$skill1' WHERE B_ID='$B_ID' AND P_ID='$P_ID' AND S_ID IN (SELECT S_ID FROM (SELECT S_ID FROM have WHERE B_ID='$B_ID' AND P_ID='$P_ID' LIMIT 1) AS temp)";
+$conn->query($update_sql);
 
-}else{
-	echo "資料不完全";
-}
-				
+$update_sql = "UPDATE have SET S_ID='$skill2' WHERE B_ID='$B_ID' AND P_ID='$P_ID' AND S_ID IN (SELECT S_ID FROM (SELECT S_ID FROM have WHERE B_ID='$B_ID' AND P_ID='$P_ID' LIMIT 1, 1) AS temp)";
+$conn->query($update_sql);
+
+$update_sql = "UPDATE have SET S_ID='$skill3' WHERE B_ID='$B_ID' AND P_ID='$P_ID' AND S_ID IN (SELECT S_ID FROM (SELECT S_ID FROM have WHERE B_ID='$B_ID' AND P_ID='$P_ID' LIMIT 2, 1) AS temp)";
+$conn->query($update_sql);
+
+echo "技能更新成功";
+
+$conn->close();
 ?>
